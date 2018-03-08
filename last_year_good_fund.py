2229e5e1617cb9a49bd6c2bf467cef49d21f19f5
宋治佳 <jia@songzhj.com>
2018-03-09 00:23:20 +0800

    crawler all fund detail

--- last_year_good_fund.py
+++ last_year_good_fund.py
@@ -2,10 +2,15 @@
 #coding=utf-8
 import time
 import json
+import crawler
+import config
 
 all_fund = None
 with open('all_fund_list') as f:
     all_fund = json.loads(f.read())
 
 # 每10s请求一次，写入文件
-with open
+with open('all_fund_detail', 'w+') as f:
+    for i in all_fund:
+        f.write(crawler.get(config.URL['DETAIL'] % (i)) + '\n')
+        time.sleep(2)
