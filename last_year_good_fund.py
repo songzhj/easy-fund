454de28a1a4109c5c711c771fb8d1c15f1cd28fb
宋治佳 <jia@songzhj.com>
2018-03-08 23:48:54 +0800

    gitignore

--- last_year_good_fund.py
+++ last_year_good_fund.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 #coding=utf-8
-
+import time
 import json
 
+all_fund = None
 with open('all_fund_list') as f:
-    print len(json.loads(f.read()))
+    all_fund = json.loads(f.read())
+
+# 每10s请求一次，写入文件
+with open
